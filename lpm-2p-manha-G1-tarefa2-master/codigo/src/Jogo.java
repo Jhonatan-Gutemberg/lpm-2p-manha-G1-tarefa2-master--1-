f94903aeff20c2d00d1f1a310f920c4d313fe6aa
Jhonatan-Gutemberg <mecjgr@hotmail.com>
2023-10-15 16:43:02 -0300

    Melhoria na classe jogo, correções de duplicidade.

--- lpm-2p-manha-G1-tarefa2-master/codigo/src/Jogo.java
+++ lpm-2p-manha-G1-tarefa2-master/codigo/src/Jogo.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -25,6 +28,8 @@ public class Jogo {
     private int[][] placar;
     private Map<Integer, Integer> jogosPorEquipe;
     private List<Jogo> jogosCadastrados = new ArrayList<>();
+    private Set<Jogo> jogosCadastrado = new HashSet<>();
+
     String arquivojogos = "jogos.csv";
     String arquivoequipes = "equipes.csv";
     int idEquipe = 1;
@@ -116,10 +121,13 @@ public class Jogo {
         System.out.print("Digite o número da equipe B: ");
         equipeB = scanner.nextInt();
 
-        if (jogoJaCadastrado(equipeA, equipeB)) {
+        Jogo novoJogo = new Jogo(dataDoJogo, equipeA, equipeB);
+
+        if (jogosCadastrado.contains(novoJogo)) {
             System.out.println("Esse jogo já foi cadastrado.");
             return;
         }
+        
 
         if (equipeA == equipeB) {
             System.out.println("Uma equipe não pode jogar consigo mesma.");
@@ -146,12 +154,29 @@ public class Jogo {
                 escreverNovoJogo(dataDoJogo, equipeA, equipeB);
                 atualizarContagemJogos(equipeA);
                 atualizarContagemJogos(equipeB);
+                jogosCadastrado.add(novoJogo);
             } catch (IOException e) {
                 System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
             }
         } else {
             System.out.println("ID da equipe não encontrado.");
         }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Jogo jogo = (Jogo) obj;
+        return (equipeA == jogo.equipeA && equipeB == jogo.equipeB) ||
+                (equipeA == jogo.equipeB && equipeB == jogo.equipeA);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Math.min(equipeA, equipeB), Math.max(equipeA, equipeB));
     }
 
     private boolean equipesPodemJogar(int equipe) {
@@ -207,33 +232,44 @@ public class Jogo {
         return null;
     }
 
-    public void escreverNovoJogo(String dataDoJogo, int equipeA, int equipeB) throws IOException {
-        String nomeEquipeA = obterNomeEquipe(equipeA);
-        String nomeEquipeB = obterNomeEquipe(equipeB);
+   /**
+ * Registra um novo jogo no arquivo de jogos.
+ *
+ * Este método permite o registro de um novo jogo no arquivo de jogos, fornecendo a data
+ * do jogo, o número da equipe A e o número da equipe B. Os pontos do jogo são definidos
+ * como zero por padrão. Se as equipes forem válidas, o jogo é adicionado ao arquivo.
+ * 
+ * @param dataDoJogo A data do jogo no formato "dd/MM/yyyy".
+ * @param equipeA O número da equipe A.
+ * @param equipeB O número da equipe B.
+ * 
+ * @throws IOException Se ocorrer um erro de leitura ou escrita do arquivo.
+ */
+public void escreverNovoJogo(String dataDoJogo, int equipeA, int equipeB) throws IOException {
+    String nomeEquipeA = obterNomeEquipe(equipeA);
+    String nomeEquipeB = obterNomeEquipe(equipeB);
 
-        if (nomeEquipeA != null && nomeEquipeB != null) {
+    if (nomeEquipeA != null && nomeEquipeB != null) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = stringToDate(dataDoJogo);
 
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date data = stringToDate(dataDoJogo);
+        String novaLinha = String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",
+                dateFormat.format(data), nomeEquipeA, nomeEquipeB, "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                "0");
 
-            String novaLinha = String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",
-                    dateFormat.format(data), nomeEquipeA, nomeEquipeB, "0", "0", "0", "0", "0", "0", "0", "0", "0",
-                    "0");
+        FileWriter arquivoWriter = new FileWriter(arquivojogos, true);
+        BufferedWriter bufferedWriter = new BufferedWriter(arquivoWriter);
+        bufferedWriter.write(novaLinha);
+        bufferedWriter.newLine();
 
-            FileWriter arquivoWriter = new FileWriter(arquivojogos, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(arquivoWriter);
+        bufferedWriter.close();
+        arquivoWriter.close();
 
-            bufferedWriter.write(novaLinha);
-            bufferedWriter.newLine();
-
-            bufferedWriter.close();
-            arquivoWriter.close();
-
-            System.out.println("Novo jogo adicionado ao arquivo.");
-        } else {
-            System.out.println("ID da equipe não encontrado.");
-        }
+        System.out.println("Novo jogo adicionado ao arquivo.");
+    } else {
+        System.out.println("ID da equipe não encontrado.");
     }
+}
 
     /**
      * Método para criar o arquivo .csv.
@@ -286,7 +322,24 @@ public class Jogo {
             System.out.println(e);
         }
     }
-
+  
+/**
+ * Permite ao usuário registrar a pontuação de um jogo previamente cadastrado.
+ * O método solicita ao usuário que insira o número do jogo a ser pontuado e,
+ * em seguida, solicita a pontuação para cada equipe e tempo. Os dados da partida
+ * são atualizados e armazenados no arquivo de jogos.
+ *
+ * Este método realiza as seguintes etapas:
+ * 1. Exibe a lista de jogos cadastrados para ajudar o usuário a escolher um jogo.
+ * 2. Solicita ao usuário o número do jogo que deseja pontuar.
+ * 3. Verifica se o número do jogo é válido (dentro dos limites da lista de jogos).
+ * 4. Recupera as informações do jogo selecionado.
+ * 5. Solicita a pontuação para cada equipe e tempo, incluindo a prorrogação.
+ * 6. Atualiza os dados da partida no arquivo de jogos.
+ * 7. Exibe o resultado da partida e salva a pontuação no arquivo.
+ *
+ * @throws IOException Se ocorrer um erro ao ler/escrever no arquivo de jogos.
+ */
     public void pontuarJogo() {
         scanner = new Scanner(System.in);
 
@@ -394,32 +447,7 @@ public class Jogo {
         return null;
     }
 
-    public String obterequipe(String equipeA, String equipeB) {
-        try {
-            FileReader arquivoReader = new FileReader(arquivoequipes);
-            try (BufferedReader bufferedLeitura = new BufferedReader(arquivoReader)) {
-                String linha;
-                int idLinha = 1;
-
-                while ((linha = bufferedLeitura.readLine()) != null) {
-                    if (idLinha == idEquipe) {
-
-                        return linha.split(",")[0];
-                    }
-                    idLinha++;
-                }
-
-                bufferedLeitura.close();
-            }
-
-            arquivoReader.close();
-        } catch (IOException e) {
-            System.err.println("Erro ao ler o arquivo de equipes: " + e.getMessage());
-        }
-
-        return null;
-    }
-
+   
     /**
      * Método para inserir a pontuação do placar do jogo.
      * O usuário é solicitado a inserir os pontos para cada equipe e tempo.
